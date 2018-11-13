90b947d91d200d1c0e0fb97db1b22248cbba5057
Samuel Cornet <samuel@noukod.com>
2018-11-13 18:43:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,15 +76,15 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
   end
 
-  # def self.find_by_name(name)
-  #   sql = <<-SQL
-  #   SELECT * FROM students WHERE name = ?
-  #   SQL
-  # 
-  #   DB[:conn].execute(sql,name).map do |row|
-  #     new_from_db(row)
-  #   end.first
-  # end
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM students WHERE name = ?
+    SQL
+  
+    DB[:conn].execute(sql,name).map do |row|
+      new_from_db(row)
+    end.first
+  end
 
   def self.all_students_in_grade_X
       sql = <<-SQL
