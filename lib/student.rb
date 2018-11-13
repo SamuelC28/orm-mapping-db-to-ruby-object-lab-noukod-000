03c421919969bf1ff566f3b955bb43778c9f2a35
Samuel Cornet <samuel@noukod.com>
2018-11-13 18:40:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,7 +86,7 @@ class Student
     end.first
   end
 
-  def self.all_students_in_grade_X x
+  def self.all_students_in_grade_X 
       sql = <<-SQL
       SELECT * FROM students WHERE grade = ?
       SQL
