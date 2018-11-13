0552f03c13b03050860609ed311d58a5b2d37ff2
Samuel Cornet <samuel@noukod.com>
2018-11-13 18:03:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,11 +13,19 @@ class Student
     sql = <<-SQL
     SELECT * FROM students
     SQL
+    DB[:conn].execute(sql).map do |row|
+         new_from_db row
+       end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = ?
+    SQL
+   
+    DB[:conn].execute(sql,"9").map do |row|
+       new_from_db row
+    end
   end
 
   def save
@@ -47,18 +55,7 @@ class Student
   end
 end
 # ......
-#
-#
-#
-#   def self.all
-#     sql = <<-SQL
-#     SELECT * FROM students
-#     SQL
-#
-#     DB[:conn].execute(sql).map do |row|
-#       create row
-#     end
-#    end
+
 #    # ..............
 #    def self.all_students_in_grade_9
 #       sql = <<-SQL
